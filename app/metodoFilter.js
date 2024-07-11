cc183ac2bc84721d2ab2afef5207162dff9997c4
Rosangela <96451367+Rommendes@users.noreply.github.com>
2024-07-11 12:51:52 -0300

    up

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,45 +1,4 @@
-//BOTÕES: filtrar por categoria
 
-// const botaoes = document.querySelectorAll(".btn");
-// botaoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
-
-// function filtrarLivros() {
-//     const elementoBTN = document.getElementById(this.id);//AQUI SEI EXATAMENTE O BOTÃO QUE ESTOU CLICANDo
-    
-//     const categoria = elementoBTN.value
-
-//     let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
-
-//     exibirOsLivrosNaTela(livrosFiltrados)//No métodoForEach coloquei innerHTML vazio para que apareçam os livros filtrados.
-//     if(categoria == 'disponivel'){
-//         const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
-//         exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
-//     }
-
-// }
-            
-// function filtrarPorCategoria(categoria){
-//     return livros.filter(livro => livro.categoria == categoria)
-// }
-
-// function filtrarPorDisponibilidade(){
-//     return livros.filter(livro => livro.quantidade > 0)
-// }
-// //     let livrosFiltrados = categoria == "disponivel" ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
-
-// //     exibirOsLivrosNaTela(livrosFiltrados) //No métodoForEach coloquei innerHTML vazio para que apareçam os livros filtrados.
-// //     if(categoria == "disponivel") {
-// //         exibirValorTotalDosLivrosDisponiveisNaTela()
-// //     }
-
-
-// function exibirComValorTotalDeLIvrosDisponiveisNaTela(valorTotal){
-//     elementoComValorTotalDeLIvrosDisponiveis.innerHTML = `
-//     <div class="livros__disponiveis">
-//       <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
-//     </div>
-//     `
-// }
 const botoes = document.querySelectorAll('.btn')
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
